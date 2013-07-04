27cc10efc748ff4e746fac124657a959ac61a4e0
ubaid <ubaidullah@darussalampk.com>
2013-07-04 18:55:52 +0500

    Bug #27 Add Product Overview

--- themes/dtech_second/views/product/_product_detail_data.php
+++ themes/dtech_second/views/product/_product_detail_data.php
@@ -1,5 +1,6 @@
 <div class="right_detail">
     <h1><?php echo $product->product_name; ?></h1>
+    <h3 style="font-size: 14px; margin-top: 8px"><?php echo $product->product_overview; ?></h3>
 
     <h2>
 
@@ -64,10 +65,9 @@
                                 dtech_new.loadCartAgain("' . $this->createUrl("/web/cart/loadCart") . '");
                                
                             });    
-                      ', 
-                      'class' => 'add_to_cart_arrow',
-                      
-                       ));
+                      ',
+            'class' => 'add_to_cart_arrow',
+        ));
         ?>
 
 
@@ -243,8 +243,8 @@
     ?>
     <section>
         Item Code:    <?php
-    echo isset($product->productProfile[0]->item_code) ? $product->productProfile[0]->item_code : "";
-    ?>
+        echo isset($product->productProfile[0]->item_code) ? $product->productProfile[0]->item_code : "";
+        ?>
     </section>
     <section>Category: <?php
         $cat_count = 0;
@@ -256,7 +256,7 @@
             }
             $cat_count++;
         }
-    ?>
+        ?>
     </section>
     <section>
         <?php
--- themes/dtech_second/views/quran/_product_detail_data.php
+++ themes/dtech_second/views/quran/_product_detail_data.php
@@ -1,6 +1,6 @@
 <div class="right_detail">
     <h1><?php echo $product->product_name; ?></h1>
-
+    <h3 style="font-size: 14px; margin-top: 8px"><?php echo $product->product_overview; ?></h3>
     <h2>
 
         <?php
@@ -143,7 +143,7 @@
         if ($product->productProfile[0]->quantity == 0) {
             echo "No";
         } else {
-           echo "Yes";
+            echo "Yes";
         }
         ?>
     </section>
