3267861a4225fc46cf6902d78c29af6953303180
ubaid <ubaidullah@darussalampk.com>
2013-07-15 16:00:26 +0500

    working on multiple langugae using cphpmessagesource

---
+++ protected/messages/ar/common.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * this is the file to 
+ * store the key value pair of 
+ * translated form of the site in arabic 
+ * language
+ * common part of the application
+ */
+
+return array(
+//     main four parent categories
+//     Landing page content
+    "Books" => "كتاب",
+    "Quran" => "القرآن",
+    "Educational Toys" => "ألعاب تعليمية",
+    "Other Items" => "الأصناف الأخرى",
+    "Popular" => "شعبي ",
+    "Select Your Country" => "حدد بلدك",
+    "Remember me" => " تذكرني",
+    "Your authentic source of knowledge" => "مصدر أصيلة معرفتك",
+    "Members Shopping" => "تسوق الأعضاء",
+    "Active Members" => "الأعضاء النشطين",
+    "Shop Now" => "تسوق الآن",
+);
+?>
---
+++ protected/messages/ar/header_footer.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * this is the file to 
+ * store the key value pair of 
+ * translated form of the site in arabic 
+ * language 
+ * 
+ * header, banner and footer  areas of the application
+ */
+
+return array(
+    //header and banner part of the apps
+
+    "Sign Up" => "قم بالتسجيل",
+    "Login" => "قم بالتسجيل",
+    "Logout" => "تسجيل الخروج",
+    "Contact Us" => "اتصل بنا",
+    "Blog" => "بلوق",
+    "User Login" => "دخول المستخدم",
+    "My Profile" => "صفحتي",
+    "Change Password" => "تغيير كلمة المرور",
+    "Order History" => "تاريخ النظام",
+    "type here" => "اكتب هنا",
+    "Search" => "بحث",
+    "wishlist" => "قائمة الامنيات",
+    "My Shopping Bag" => "بلدي حقيبة تسوق",
+    "Total" => "مجموع",
+    "CHECKOUT" => "الخروج",
+    "Logins" => "",
+);
+?>
