01bddb73625a83d1b258bfbfd8d989dd85af10f5
ubaid <ubaidullah@darussalampk.com>
2013-07-16 15:54:00 +0500

    working on multiple langugae using cphpmessagesource

---
+++ protected/messages/ar/product_detail.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+return array(
+    "Book Description" => "وصف الكتاب",
+    "Most Recent Customer Reviews" => "معظم الزوار آراء العملاء",
+);
+
+?>
