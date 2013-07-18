6b165e49333a193bb4fa33ff711e8669d86fec80
aliabbas <aliabbas@localhost>
2013-07-18 09:24:29 +0500

    bugs

--- protected/models/ProductProfile.php
+++ protected/models/ProductProfile.php
@@ -315,5 +315,40 @@ class ProductProfile extends DTActiveRecord {
         return parent::afterFind();
     }
 
+    /**
+     * for updating english record
+     * on each case
+     * when parent record is updated
+     */
+    public function attachBehaviors($behaviors) {
+
+        $bhv = array('ml' => array(
+                'class' => 'MultilingualBehavior',
+                'langClassName' => 'ProductProfileLang',
+                'langTableName' => 'product_profile_lang',
+                'langForeignKey' => 'product_profile_id',
+                //'langField' => 'lang_id',
+                'localizedAttributes' => array(
+                    'title',
+                ), //attributes of the model to be translated
+                'localizedPrefix' => '',
+                'languages' => Yii::app()->params['translatedLanguages'], // array of your translated languages. Example : array('fr' => 'Français', 'en' => 'English')
+                'defaultLanguage' => Yii::app()->params['defaultLanguage'], //your main language. Example : 'fr'
+            //'createScenario' => 'insert',
+            //'localizedRelation' => 'postLangs',
+            //'multilangRelation' => 'multilangPost',
+            //'forceOverwrite' => false,
+            //'forceDelete' => true, 
+            //'dynamicLangClass' => true, //Set to true if you don't want to create a 'PostLang.php' in your models folder
+        ));
+
+        if (Yii::app()->request->getQuery('id') == "") {
+            $behaviors = array_merge($behaviors, $bhv);
+        }
+
+
+        parent::attachBehaviors($behaviors);
+        return true;
+    }
 
 }
