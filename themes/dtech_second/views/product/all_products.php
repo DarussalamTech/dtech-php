b175e86f239dbb3e02dfbbb78a36ebe7aed16724
ubaid <ubaidullah@darussalampk.com>
2013-07-27 11:17:25 +0500

    product listing page

--- themes/dtech_second/views/product/all_products.php
+++ themes/dtech_second/views/product/all_products.php
@@ -1,215 +1,23 @@
 <?php
-$cs = Yii::app()->clientScript;
-$cs->registerCssFile(Yii::app()->theme->baseUrl . '/css/list.css');
+
+/**
+ * rendering all products listing
+ */
+echo $this->renderPartial('//product/product_listing', array(
+    'products' => $products,
+    'dataProvider' => $dataProvider,
+    'allCate' => $allCate)
+);
+
+
+/**
+ * for checking ajax based link
+ */
+$page = 1;
+if (isset($_REQUEST['Product_page'])) {
+    $page = $_REQUEST['Product_page'];
+}
+Yii::app()->clientScript->registerScript('change_cat_script', '
+        dtech.loadallPrdoucts_Cat("' . $this->createUrl("/web/product/allproducts", array("Product_page" => $page)) . '");
+    ', CClientScript::POS_READY);
 ?>
-<div id="main_features_part">
-    <div id="features_part">
-        <div id="featured_books">
-            <div id="right_featured">
-                <div class="list_all">
-                    <h1>List All</h1>
-                    <div class="listing">
-                        <p><a href="#">
-                                <img src="<?php echo Yii::app()->theme->baseUrl ?>/images/bottom_list_03.jpg"> Quran</a>
-                        </p>
-                        <div class="inner_list">
-                            <li><a href="#">ABC</a></li>
-                            <li><a href="#">DEF</a></li>
-                            <li><a href="#">GHI</a></li>
-                        </div>
-                    </div>
-                    <div class="listing">
-                        <p><a href="#"><img src="<?php echo Yii::app()->theme->baseUrl ?>/images/list_arrow_03.jpg"> Books</a></p>
-                    </div>
-                    <div class="listing">
-                        <p><a href="#"><img src="<?php echo Yii::app()->theme->baseUrl ?>/images/list_arrow_03.jpg"> Books</a></p>
-                    </div>
-                    <div class="listing">
-                        <p><a href="#"><img src="<?php echo Yii::app()->theme->baseUrl ?>/images/list_arrow_03.jpg"> Books</a></p>
-                    </div>
-                    <div class="listing">
-                        <p><a href="#"><img src="<?php echo Yii::app()->theme->baseUrl ?>/images/list_arrow_03.jpg"> Books</a></p>
-                    </div>
-                    <div class="listing">
-                        <p><a href="#"><img src="<?php echo Yii::app()->theme->baseUrl ?>/images/list_arrow_03.jpg"> Books</a></p>
-                    </div>
-                </div>
-                <div id="tweets">
-                    <h3><img src="<?php echo Yii::app()->theme->baseUrl ?>/images/tweets_img_03.jpg"><span>Tweets</span></h3>
-                    <p>http://en.wikipedia.org/wiki/ Tweets_For_My_Sweet</p>
-                    <p>http://en.wikipedia.org/wiki/ Tweets_For_My_Sweet</p>
-                    <p>http://en.wikipedia.org/wiki/ Tweets_For_My_Sweet</p>
-                    <p>http://en.wikipedia.org/wiki/ Tweets_For_My_Sweet</p>
-                    <p>http://en.wikipedia.org/wiki/ Tweets_For_My_Sweet</p>
-                    <article>http://en.wikipedia.org/wiki/ Tweets_For_My_Sweet</article>
-                </div>
-                <div class="right_under_content">
-                    <h5>Bookshelf Favorites</h5>
-                    <h6>Save <img src="<?php echo Yii::app()->theme->baseUrl ?>/images/up_to_03.jpg"> 50%</h6>
-                    <article>on Selected Books</article>
-                    <p>&gt;Learn more</p>
-                    <img width="210px" src="<?php echo Yii::app()->theme->baseUrl ?>/images/book_with_pages_img_07.png">
-                </div>
-            </div>
-            <div id="list_featured">
-                <h6>Latest</h6>
-                <div class="featured_cover">
-                    <div class="featured_cover_part">
-                        <img src="<?php echo Yii::app()->theme->baseUrl ?>/images/gems_and_jewels_03.jpg">
-                        <h2>Gems and Jewels</h2>
-                        <p>Lorem ipsum color sit bla bla thhm ipoum deona</p>
-                    </div>
-                    <div class="featured_bottom">
-                        <span>$185.99</span>
-                        <div class="white_basket">
-                            <img src="<?php echo Yii::app()->theme->baseUrl ?>/images/white_basket_03.jpg">
-                        </div>
-                    </div>
-                </div>
-                <div class="featured_cover">
-                    <div class="featured_cover_part">
-                        <img src="<?php echo Yii::app()->theme->baseUrl ?>/images/precious_pearl_03.jpg">
-                        <h2>The Precious Pearl</h2>
-                        <p>Lorem ipsum color sit bla bla thhm ipoum deona</p>
-                    </div>
-                    <div class="featured_bottom">
-                        <span>$185.99</span>
-                        <div class="white_basket">
-                            <img src="<?php echo Yii::app()->theme->baseUrl ?>/images/white_basket_03.jpg">
-                        </div>
-                    </div>
-                </div>
-                <div class="featured_cover">
-                    <div class="featured_cover_part">
-                        <img src="<?php echo Yii::app()->theme->baseUrl ?>/images/happiness_03.jpg">
-                        <h2>Happiness</h2>
-                        <p>Lorem ipsum color sit bla bla thhm ipoum deona</p>
-                    </div>
-                    <div class="featured_bottom">
-                        <span>$185.99</span>
-                        <div class="white_basket">
-                            <img src="<?php echo Yii::app()->theme->baseUrl ?>/images/white_basket_03.jpg">
-                        </div>
-                    </div>
-                </div>
-                <div class="featured_cover">
-                    <div class="featured_cover_part">
-                        <img src="<?php echo Yii::app()->theme->baseUrl ?>/images/gems_and_jewels_03.jpg">
-                        <h2>Gems and Jewels</h2>
-                        <p>Lorem ipsum color sit bla bla thhm ipoum deona</p>
-                    </div>
-                    <div class="featured_bottom">
-                        <span>$185.99</span>
-                        <div class="white_basket">
-                            <img src="<?php echo Yii::app()->theme->baseUrl ?>/images/white_basket_03.jpg">
-                        </div>
-                    </div>
-                </div>
-                <div class="featured_cover">
-                    <div class="featured_cover_part">
-                        <img src="<?php echo Yii::app()->theme->baseUrl ?>/images/precious_pearl_03.jpg">
-                        <h2>The Precious Pearl</h2>
-                        <p>Lorem ipsum color sit bla bla thhm ipoum deona</p>
-                    </div>
-                    <div class="featured_bottom">
-                        <span>$185.99</span>
-                        <div class="white_basket">
-                            <img src="<?php echo Yii::app()->theme->baseUrl ?>/images/white_basket_03.jpg">
-                        </div>
-                    </div>
-                </div>
-                <div class="featured_cover">
-                    <div class="featured_cover_part">
-                        <img src="<?php echo Yii::app()->theme->baseUrl ?>/images/happiness_03.jpg">
-                        <h2>Happiness</h2>
-                        <p>Lorem ipsum color sit bla bla thhm ipoum deona</p>
-                    </div>
-                    <div class="featured_bottom">
-                        <span>$185.99</span>
-                        <div class="white_basket">
-                            <img src="<?php echo Yii::app()->theme->baseUrl ?>/images/white_basket_03.jpg">
-                        </div>
-                    </div>
-                </div>
-                <div class="featured_cover">
-                    <div class="featured_cover_part">
-                        <img src="<?php echo Yii::app()->theme->baseUrl ?>/images/gems_and_jewels_03.jpg">
-                        <h2>Gems and Jewels</h2>
-                        <p>Lorem ipsum color sit bla bla thhm ipoum deona</p>
-                    </div>
-                    <div class="featured_bottom">
-                        <span>$185.99</span>
-                        <div class="white_basket">
-                            <img src="<?php echo Yii::app()->theme->baseUrl ?>/images/white_basket_03.jpg">
-                        </div>
-                    </div>
-                </div>
-                <div class="featured_cover">
-                    <div class="featured_cover_part">
-                        <img src="<?php echo Yii::app()->theme->baseUrl ?>/images/precious_pearl_03.jpg">
-                        <h2>The Precious Pearl</h2>
-                        <p>Lorem ipsum color sit bla bla thhm ipoum deona</p>
-                    </div>
-                    <div class="featured_bottom">
-                        <span>$185.99</span>
-                        <div class="white_basket">
-                            <img src="<?php echo Yii::app()->theme->baseUrl ?>/images/white_basket_03.jpg">
-                        </div>
-                    </div>
-                </div>
-                <div class="featured_cover">
-                    <div class="featured_cover_part">
-                        <img src="<?php echo Yii::app()->theme->baseUrl ?>/images/happiness_03.jpg">
-                        <h2>Happiness</h2>
-                        <p>Lorem ipsum color sit bla bla thhm ipoum deona</p>
-                    </div>
-                    <div class="featured_bottom">
-                        <span>$185.99</span>
-                        <div class="white_basket">
-                            <img src="<?php echo Yii::app()->theme->baseUrl ?>/images/white_basket_03.jpg">
-                        </div>
-                    </div>
-                </div>
-                <div class="featured_cover">
-                    <div class="featured_cover_part">
-                        <img src="<?php echo Yii::app()->theme->baseUrl ?>/images/gems_and_jewels_03.jpg">
-                        <h2>Gems and Jewels</h2>
-                        <p>Lorem ipsum color sit bla bla thhm ipoum deona</p>
-                    </div>
-                    <div class="featured_bottom">
-                        <span>$185.99</span>
-                        <div class="white_basket">
-                            <img src="<?php echo Yii::app()->theme->baseUrl ?>/images/white_basket_03.jpg">
-                        </div>
-                    </div>
-                </div>
-                <div class="featured_cover">
-                    <div class="featured_cover_part">
-                        <img src="<?php echo Yii::app()->theme->baseUrl ?>/images/precious_pearl_03.jpg">
-                        <h2>The Precious Pearl</h2>
-                        <p>Lorem ipsum color sit bla bla thhm ipoum deona</p>
-                    </div>
-                    <div class="featured_bottom">
-                        <span>$185.99</span>
-                        <div class="white_basket">
-                            <img src="<?php echo Yii::app()->theme->baseUrl ?>/images/white_basket_03.jpg">
-                        </div>
-                    </div>
-                </div>
-                <div class="featured_cover">
-                    <div class="featured_cover_part">
-                        <img src="<?php echo Yii::app()->theme->baseUrl ?>/images/happiness_03.jpg">
-                        <h2>Happiness</h2>
-                        <p>Lorem ipsum color sit bla bla thhm ipoum deona</p>
-                    </div>
-                    <div class="featured_bottom">
-                        <span>$185.99</span>
-                        <div class="white_basket">
-                            <img src="<?php echo Yii::app()->theme->baseUrl ?>/images/white_basket_03.jpg">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
