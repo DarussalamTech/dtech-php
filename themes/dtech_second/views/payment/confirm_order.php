7329f0ec15a2ee6252119cc054121aa4098a4738
ubaid <ubaidullah@darussalampk.com>
2013-08-03 14:29:15 +0500

    Feature  # 150
    New Design of Payment method and shipping address method
    Confirmed ORDER placement page

--- themes/dtech_second/views/payment/confirm_order.php
+++ themes/dtech_second/views/payment/confirm_order.php
@@ -1,21 +1,48 @@
-<div id="book_content">
-    <div id="book_main_content">
-        <?php $this->renderPartial("/product/_subheader"); ?>
-    </div>
-</div>
 <?php
+Yii::app()->clientScript->registerCssFile(Yii::app()->theme->baseUrl . '/css/form.css');
 ?>
-<div id="shopping_cart" style="height:308px;text-align:center;  ">
-    <div id="main_shopping_cart">
-        <div class="left_right_cart">
-            Your Order successfully completed....
+<div id="login_content">
+
+
+    <div class="secure_form">
+        <div class="payment_method_big_img">
+            <?php
+            echo CHtml::image(Yii::app()->theme->baseUrl . "/images/place_order_img_03.png", 'Payment_method', array('class' => "payment_method_big_img"));
+            ?>
+        </div>
+        <div class="secure_payment">
+            <div class="secure_heading">
+                <h1>Secure Payment</h1>
+                <p>This is a secure 128 bit SSL encrypter payment</p>
+            </div>
+            <div class="payment_bg">
+                <article><?php echo CHtml::image(Yii::app()->theme->baseUrl . '/images/tick_payment_img_03.png'); ?></article>
+                <section><?php echo CHtml::image(Yii::app()->theme->baseUrl . '/images/tick_payment_img_03.png'); ?></section>
+                <h3>3</h3>
+                <span>Personal Information</span>
+                <h5>Billing Address</h5>
+                <h2>Shipping Address.</h2>
+            </div>
+        </div>
+
+        <div class="confirm_order">
+            Your Order has  successfully completed....
             <?php if (Yii::app()->user->hasFlash('orderMail')) { ?>
                 <div class="flash-success" style="color:green">
                     <?php echo '<br/><tt>' . Yii::app()->user->getFlash('orderMail') . '</tt>'; ?>
                 </div>
             <?php } ?>
         </div>
-    </div>                                        
+    </div>
+
 </div>
-<?php
-?>
+<style>
+    .credit_card_fields,.pay_list,.manual_list{
+        display: none;
+    }
+    <?php
+    if ($model->payment_method == "Credit Card") {
+        echo ".credit_card_fields{display:block;}";
+    }
+    ?>
+</style>
