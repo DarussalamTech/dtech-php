923cacaa542d9b0827278412f2fccbee774ee7d2
ubaid <ubaidullah@darussalampk.com>
2013-08-28 14:29:53 +0500

    deleting all extra themes....and images to light the code

--- themes/default/views/page/view_page.php
+++
@@ -1,9 +0,0 @@
-<div id="book_content">
-    <div id="book_main_content">
-        <?php $this->renderPartial("//product/_subheader"); ?>
-        <div id="right_main_content">
-            <h2><?php echo $page->title; ?></h2>
-            <?php echo $page->content; ?>
-        </div>
-    </div>
-</div>
