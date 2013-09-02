7a78b868d2da127132e53260816672576d6ca187
aliabbas <aliabbas@localhost>
2013-09-02 10:00:09 +0500

    Order status

--- protected/views/order/index.php
+++ protected/views/order/index.php
@@ -30,13 +30,14 @@ $('.search-form form').submit(function(){
 <p>
     <b>Information:</b>
     <br/>
-    If Order Status changes Completed to Declined = Then Quantity will be reverted to Products
+    If Order Status changes Shipped to Canceled Or Refunded = Then Quantity will be reverted to Products
     <br/>
-    If Order Status changes process to Completed = Then Quantity will be decreased to Products
+    If Order Status changes Pending or Process to Shipped = Then Quantity will be decreased to Products
+    <br/>
+    Completed , Declined or neutral
 </p>
 
 <?php
-
 echo CHtml::openTag("div", array(
     "class" => "flash-success",
     "id" => 'flash-message',
