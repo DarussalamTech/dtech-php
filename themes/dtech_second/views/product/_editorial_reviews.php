4970733f7b23d14b91880bddfb05adedc2bd9010
ubaid <ubaidullah@darussalampk.com>
2013-09-03 14:56:51 +0500

    working on product detail page

--- themes/dtech_second/views/product/_editorial_reviews.php
+++ themes/dtech_second/views/product/_editorial_reviews.php
@@ -1,2 +1,2 @@
 <h2><?php echo Yii::t('product_detail', 'Book Description', array(), NULL, $this->currentLang); ?></h2>
-<p><?php echo $product->product_description; ?></p>
+<div class="border_detail"><p><?php echo $product->product_description; ?></p></div>
