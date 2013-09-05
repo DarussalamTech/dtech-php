7a8f71988301b3a26d7568ac3337664e03d2ce38
ubaid <ubaidullah@darussalampk.com>
2013-09-05 18:15:37 +0500

    addming order email link to customer email

--- themes/dtech_second/views/product/_product_add_to_cart.php
+++ themes/dtech_second/views/product/_product_add_to_cart.php
@@ -1,4 +1,8 @@
 <h2><?php echo $product->product_name; ?></h2>
+<h3><?php
+    echo isset($product->product_overview) ? $product->product_overview : "";
+    ?>
+</h3>
 <p><?php
     echo Yii::t('model_labels', 'ISBN', array(), NULL, $this->currentLang) . ":";
     echo isset($product->productProfile[0]->isbn) ? $product->productProfile[0]->isbn : "";
@@ -37,13 +41,13 @@ $this->widget('CStarRating', array(
 ));
 ?>
 <article>
-<?php
-if ($total_av >= 1) {
-    echo CHtml::textField('quantity', '1', array('onKeyUp' => 'javascript:totalPrice(this.value,"' . $product->productProfile[0]->price . '")', 'style' => 'width:40px', 'maxlength' => '3'));
-}
-?>
+    <?php
+    if ($total_av >= 1) {
+        echo CHtml::textField('quantity', '1', array('onKeyUp' => 'javascript:totalPrice(this.value,"' . $product->productProfile[0]->price . '")', 'style' => 'width:40px', 'maxlength' => '3'));
+    }
+    ?>
     <span id="status_available" style="display:none">
-    <?php echo CHtml::image(Yii::app()->theme->baseUrl . '/images/tick_03.jpg'); ?>
+        <?php echo CHtml::image(Yii::app()->theme->baseUrl . '/images/tick_03.jpg'); ?>
 
         <?php
         echo Yii::t('model_labels', ' Available in this quantity', array(), NULL, $this->currentLang) . ":";
@@ -58,9 +62,9 @@ if ($total_av >= 1) {
     </span>
 </article>
 <div class="detail_shop_now">
-<?php
-if ($total_av > 1) {
-    echo CHtml::button(Yii::t('common', 'SHOP NOW', array(), NULL, $this->currentLang), array('onclick' => '
+    <?php
+    if ($total_av > 1) {
+        echo CHtml::button(Yii::t('common', 'SHOP NOW', array(), NULL, $this->currentLang), array('onclick' => '
                             jQuery("#loading").show();
                             jQuery("#status_available").hide();  
                             jQuery("#status_un_available").hide();  
@@ -87,11 +91,11 @@ if ($total_av > 1) {
                                
                             });    
                       ',
-        'class' => 'add_to_cart_arrow',
-    ));
-} else {
-    if (!empty(Yii::app()->user->id)) {
-        echo CHtml::button(Yii::t('common', 'Email me when available', array(), NULL, $this->currentLang), array('onclick' => '
+            'class' => 'add_to_cart_arrow',
+        ));
+    } else {
+        if (!empty(Yii::app()->user->id)) {
+            echo CHtml::button(Yii::t('common', 'Email me when available', array(), NULL, $this->currentLang), array('onclick' => '
                                 dtech_new.loadWaitmsg();
                                jQuery("#load_subpanel_div").toggle(); 
                                jQuery.ajax({
@@ -107,16 +111,16 @@ if ($total_av > 1) {
                                         dtech.custom_alert("You will be notified by email" ,"Notification");
                                 }); 
                           ', 'class' => 'add_to_cart_arrow email_cart_arrow'));
-    } else {
+        } else {
 
-        echo CHtml::button(Yii::t('common', 'Email me when available', array(), NULL, $this->currentLang), array(
-            'onclick' => '
+            echo CHtml::button(Yii::t('common', 'Email me when available', array(), NULL, $this->currentLang), array(
+                'onclick' => '
                        window.open(
                         "' . $this->createUrl("/web/cart/emailtoAdmin", array("id" => $product->productProfile[0]->id)) . '", "" )     
                 ', 'class' => 'add_to_cart_arrow email_cart_arrow'));
+        }
     }
-}
-?>
+    ?>
 
     <?php
     echo CHtml::ajaxButton(Yii::t('common', 'ADD TO WISHLIST', array(), NULL, $this->currentLang), $this->createUrl('/cart/addtowishlist'), array('data' => array(
