96f9277c5395fd4c36b42c216eb842ceafcd3b8e
ali <ali.abbas@darussalampk.com>
2013-09-25 23:21:46 +0500

    send invitation

--- protected/views/user/send_invitation.php
+++ protected/views/user/send_invitation.php
@@ -4,9 +4,9 @@ $panel = array();
 $counter = 1;
 foreach($users as $user){
     $content = "<div id='sender_".$counter."' style='border:1px solid black;padding-left:5px;margin-bottom:10px;'>";
-    $content.= "<h2>Group ".$counter."</h2>";
+    $content.= "<h2><a href='javascript:void(0);' onclick='dtech.checkAllGroupBox(this)' >Check/Uncheck </a>Group ".$counter."</h2>";
     foreach($user as $s){
-        $content.= "<span>".$s['user_email']."</span>";
+        $content.= "<span><input type='checkbox' class='user_".$counter."' value='".$s['user_email']."' />".$s['user_email']."</span>";
         $content.= "<span class='hidden' style='display:none'>".$s['user_id']."</span>";
         $content.= "<br/>";
     }
