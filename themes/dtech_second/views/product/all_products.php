66f1bd5616970c6bdaef2613a06fb2b1f1e55b27
aliabbas <aliabbas@localhost>
2013-11-18 16:06:25 +0500

    extra code will be removed

--- themes/dtech_second/views/product/all_products.php
+++ themes/dtech_second/views/product/all_products.php
@@ -9,15 +9,4 @@ echo $this->renderPartial('//product/product_listing', array(
     'allCate' => $allCate)
 );
 
-
-/**
- * for checking ajax based link
- */
-$page = 1;
-if (isset($_REQUEST['Product_page'])) {
-    $page = $_REQUEST['Product_page'];
-}
-Yii::app()->clientScript->registerScript('change_cat_script', '
-        dtech.loadallPrdoucts_Cat("' . $this->createUrl("/web/product/allproducts", array("Product_page" => $page)) . '");
-    ', CClientScript::POS_READY);
 ?>
