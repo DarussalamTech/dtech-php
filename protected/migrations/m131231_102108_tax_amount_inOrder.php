1078b682297068935ef6e2b91f314aaaad57a65e
aliabbas <aliabbas@localhost>
2014-01-01 14:47:48 +0500

    shipping and tax on

--- protected/migrations/m131231_102108_tax_amount_inOrder.php
+++ protected/migrations/m131231_102108_tax_amount_inOrder.php
@@ -7,7 +7,7 @@ class m131231_102108_tax_amount_inOrder extends DTDbMigration {
 
     public function up() {
         $table = "order";
-        $this->addColumn($table, "tax_amount", "double(12,3) DEFAULT NULL after shipping_price");
+        $this->addColumn($table, "tax_amount", "double(12,3) DEFAULT 0 after shipping_price");
     }
 
     public function down() {
