95747fc2edafcd97c4bd2db386209862e3b014c5
root <root@hamza.(none)>
2014-02-18 18:32:37 +0500

    commit

--- protected/migrations/m140218_054724_createing_productprofile_ksa.php
+++ protected/migrations/m140218_054724_createing_productprofile_ksa.php
@@ -7,7 +7,7 @@ class m140218_054724_createing_productprofile_ksa extends DTDbMigration {
         $ryd = $this->getRiyadhCityId();
         $table = 'product';
         $table2 = 'product_profile';
-        $prod_prof = $this->getQueryAll("SELECT t.product_id,title,item_code,language_id,quantity,no_of_pages,binding,printing,paper,dimension,weight,weight_unit,is_shippable,edition,color,attribute,attribute,compiler_id,translator_id,isbn 	 FROM `product_profile` as t left join product on t.product_id=product.product_id where product.city_id=" . $lhr[0]);
+        $prod_prof = $this->getQueryAll("SELECT t.product_id,title,item_code,language_id,quantity,no_of_pages,binding,printing,paper,dimension,weight,weight_unit,is_shippable,edition,color,attribute,attribute,compiler_id,translator_id,is_featured,isbn 	 FROM `product_profile` as t left join product on t.product_id=product.product_id where product.city_id=" . $lhr[0]);
         $prod_lhr = $this->getQueryAll("SELECT product_id FROM `product` where city_id=" . $lhr[0]);
         $prod_ryd = $this->getQueryAll("SELECT product_id FROM `product` where city_id=" . $ryd[0]);
 
