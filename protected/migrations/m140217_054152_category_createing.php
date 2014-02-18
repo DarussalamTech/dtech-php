88717b80a4b94e79e069734d8dc5f8ee395ec4ea
aliabbas <aliabbas@localhost>
2014-02-18 15:29:20 +0500

    code review

--- protected/migrations/m140217_054152_category_createing.php
+++ protected/migrations/m140217_054152_category_createing.php
@@ -1,31 +1,26 @@
 <?php
 
+/**
+ * For parent category insertion
+ */
 class m140217_054152_category_createing extends DTDbMigration {
 
     public function up() {
         $lhr = $this->getLahoreCityId();
         $ryd = $this->getRiyadhCityId();
         $table = 'categories';
-        //SELECT category_id,category_name FROM `categories` where city_id ='5'
-        //select category_name from categories where city_id=5 and category_name in(SELECT category_name FROM `categories` where category_id in (select parent_id from categories))
-//            CVarDumper::dump($city_id);
 
-       
-        $dat = $this->getQueryAll("SELECT category_name,user_order,parent_id,city_id,added_date FROM `categories` where  city_id=" . $lhr[0] . " and parent_id='0' and category_name not in (select category_name from categories where city_id=" . $ryd[0] . " and parent_id=0 )" );
-        
+        $dat = $this->getQueryAll("SELECT category_name,user_order,parent_id,city_id,added_date FROM `categories` where  city_id=" . $lhr[0] . " and parent_id='0' and category_name not in (select category_name from categories where city_id=" . $ryd[0] . " and parent_id=0 )");
+
         if (!empty($dat)) {
             foreach ($dat as $columns) {
 
                 $ryd = $this->getRiyadhCityId();
                 $columns['city_id'] = $ryd[0];
-                $columns['added_date']=date('y/d/m');
+                $columns['added_date'] = time();
                 $this->insertRow($table, $columns);
             }
         }
-
-
-        
-
         return true;
     }
 
@@ -34,14 +29,4 @@ class m140217_054152_category_createing extends DTDbMigration {
         return true;
     }
 
-    /*
-      // Use safeUp/safeDown to do migration with transaction
-      public function safeUp()
-      {
-      }
-
-      public function safeDown()
-      {
-      }
-     */
 }
--- protected/migrations/m140217_121527_category_creation_completion.php
+++ protected/migrations/m140217_121527_category_creation_completion.php
@@ -7,20 +7,17 @@ class m140217_121527_category_creation_completion extends DTDbMigration {
         $lhr = $this->getLahoreCityId();
         $ryd = $this->getRiyadhCityId();
         $table = 'categories';
-        //SELECT category_id,category_name FROM `categories` where city_id ='5'
-        //select category_name from categories where city_id=5 and category_name in(SELECT category_name FROM `categories` where category_id in (select parent_id from categories))
-//            CVarDumper::dump($city_id);
 
         $data = $this->getQueryAll("SELECT category_name,user_order,parent_id,city_id,create_user_id from categories Where city_id = " . $lhr[0]);
-        //            CVarDumper::dump($data);
+
         $parent_categories = $this->getQueryAll("SELECT category_image,category_name,category_id FROM `categories` where city_id=" . $lhr[0] . " and parent_id=0 ");
         $parent_for_new = $this->getQueryAll("select category_name,category_id from categories where city_id=" . $ryd[0] . " and parent_id=0 ");
-//        CVarDumper::dump($parent_for_new);
-//        CVarDumper::dump($parent_categories);
+
         $mapping = array(
         );
 
         $counter = 0;
+        //create mapping 
         foreach ($parent_categories as $category) {
 
             foreach ($parent_for_new as $newwcat) {
@@ -34,11 +31,10 @@ class m140217_121527_category_creation_completion extends DTDbMigration {
                 }
             }
         }
-//        CVarDumper::dump($mapping);
-//        CVarDumper::dump($data);
+
+        //generating image and children categories insertion
         foreach ($data as $columns) {
 
-            $ryd = $this->getRiyadhCityId();
             $columns['city_id'] = $ryd[0];
             foreach ($mapping as $map) {
                 if ($columns['parent_id'] == $map['old']) {
@@ -53,7 +49,7 @@ class m140217_121527_category_creation_completion extends DTDbMigration {
 
                             mkdir($newpath, 0777, true);
                         }
-                        //echo $newpath;
+
                         echo "\n";
                         echo $oldpath . DIRECTORY_SEPARATOR . $map['image'];
 
@@ -74,14 +70,4 @@ class m140217_121527_category_creation_completion extends DTDbMigration {
         return true;
     }
 
-    /*
-      // Use safeUp/safeDown to do migration with transaction
-      public function safeUp()
-      {
-      }
-
-      public function safeDown()
-      {
-      }
-     */
 }
