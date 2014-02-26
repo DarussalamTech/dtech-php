1b5724a7b8daaea5983822e693330f5b7afc6512
root <root@hamza.(none)>
2014-02-26 16:38:35 +0500

    comit

---
+++ protected/migrations/m140226_104033_alter_table_productprofile.php
@@ -0,0 +1,26 @@
+<?php
+
+class m140226_104033_alter_table_productprofile extends CDbMigration {
+
+    public function up() {
+        $sql = 'ALTER TABLE product_profile CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci';
+        $this->execute($sql);
+        return true;
+    }
+
+    public function down() {
+        echo "m140226_104033_alter_table_productprofile does not support migration down.\n";
+        return true;
+    }
+
+    /*
+      // Use safeUp/safeDown to do migration with transaction
+      public function safeUp()
+      {
+      }
+
+      public function safeDown()
+      {
+      }
+     */
+}
