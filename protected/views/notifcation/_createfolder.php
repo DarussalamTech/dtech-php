e5a3d614938102f95dede821b597bd537c736108
aliabbas <aliabbas@localhost>
2014-04-22 17:32:16 +0500

    notification in menu of top

--- protected/views/notifcation/_createfolder.php
+++ protected/views/notifcation/_createfolder.php
@@ -1,3 +1,13 @@
+<div class="pading-bottom-5">
+    <div class="left_float">
+        <h1>Create Folder</h1>
+    </div>
+
+    <?php /* Convert to Monitoring Log Buttons */ ?>
+    <div class = "right_float">
+
+    </div>
+</div>
 <div class="form wide color-box-width">
 
     <?php
@@ -10,6 +20,9 @@
         <?php echo $form->labelEx($model, 'name'); ?>
         <?php echo $form->textField($model, 'name'); ?>
         <?php echo $form->error($model, 'name'); ?>
+    </div>
+    <div class="row buttons">
+        <?php echo CHtml::submitButton($model->isNewRecord ? 'Send' : 'Save', array("class" => "btn")); ?>
     </div>
     <?php $this->endWidget(); ?>
 </div>
