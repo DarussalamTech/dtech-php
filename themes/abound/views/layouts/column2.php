071948e1c45c395ae24663f0980a4d0f2a311838
aliabbas <aliabbas@localhost>
2014-06-10 16:05:04 +0500

    configuratios

--- themes/abound/views/layouts/column2.php
+++ themes/abound/views/layouts/column2.php
@@ -17,8 +17,6 @@ $this->beginContent('//layouts/main');
                     'encodeLabel' => false,
                     'items' => array(
                         array('label' => '<i class="icon icon-home"></i>  Dashboard <span class="label label-info pull-right">Dash</span>', 'url' => array('/site/index'), 'itemOptions' => array('class' => '')),
-                        
-                        
                         array("type" => "raw", 'label' => 'Notification', 'url' => $this->createUrl('/notifcation/index'), 'visible' => (Yii::app()->user->isAdmin || Yii::app()->user->isSuperAdmin) ? 1 : 0, 'linkOptions' => array("id" => "notifcations"), 'itemOptions' => array('style' => $notifications > 0 ? "font-weight:bold" : "")),
                         array('label' => 'Access Control', 'url' => $this->createUrl('/rights'), 'visible' => (Yii::app()->user->isAdmin || Yii::app()->user->isSuperAdmin) ? 1 : 0, 'itemOptions' => array('class' => '')),
                         array('label' => 'Change Password', 'url' => $this->createUrl('/user/changePassword'), 'visible' => (Yii::app()->user->isGuest) ? 0 : 1, 'itemOptions' => array('class' => '')),
@@ -30,6 +28,89 @@ $this->beginContent('//layouts/main');
                         array('label' => 'OPERATIONS', 'items' => $this->menu),
                     ),
                 ));
+                ?>
+                <?php
+                /*
+                 * If configuration controller is called
+                 * 
+                 */
+
+                if ($this->id == "configurations" ||
+                        $this->id == "cmm" ||
+                        $this->id == "confProducts" || $this->id == "dtMessages") {
+                    $link_array = array(
+                        'Settings' => '<ul class="accordion-ul">' .
+                        '<li>' . CHtml::link('General Misc', $this->createUrl('/configurations/general', array("m" => "Misc", 'type' => 'general')), array("style" => !Yii::app()->user->IsSuperuser ? "display:none" : "")) .
+                        '</li>' .
+                        '<li>' . CHtml::link('Branch Misc', array('/configurations/load',
+                            "m" => "Misc", "type" => 'other')) .
+                        '</li>' .
+                        '<li>' . CHtml::link('Payment Methods', array('/configurations/load',
+                            "m" => "PaymentMethods")) . '</li>' .
+                        '<li>' . CHtml::link('Currency Rates', array('/configurations/load',
+                            "m" => "Currency")) . '</li>' .
+                        '<li>' . CHtml::link('Tax Rates', array('/configurations/load',
+                            "m" => "TaxRates")) . '</li>' .
+                        '</ul>',
+                        'Product' => '<ul class="accordion-ul">' .
+                        '<li>' . CHtml::link('Dimensions', array('/configurations/load',
+                            "m" => "Products", 'type' => 'Dimensions')) .
+                        '</li>' .
+                        '<li>' . CHtml::link('Binding', array('/configurations/load',
+                            "m" => "Products", "type" => 'Binding')) .
+                        '</li>' .
+                        '<li>' . CHtml::link('Printing', array('/configurations/load',
+                            "m" => "Products", "type" => "Printing")) . '</li>' .
+                        '<li>' . CHtml::link('Paper', array('/configurations/load',
+                            "m" => "Products", "type" => "Paper")) . '</li>' .
+                        '<li>' . CHtml::link('Weight', array('/configurations/load',
+                            "m" => "Products", "type" => "weight")) . '</li>' .
+                        '<li>' . CHtml::link('Author', array('/author/index',
+                                ), array("style" => Yii::app()->user->IsSuperuser ? "display:none" : "")) . '</li>' .
+                        '<li>' . CHtml::link('Translator Compiler', array('/translatorCompiler/index',
+                                ), array("style" => Yii::app()->user->IsSuperuser ? "display:none" : "")) . '</li>' .
+                        '<li>' . CHtml::link('Product Custom Attributes', array('/configurations/load',
+                            "m" => "ProductAttributes", "type" => "")) . '</li>' .
+                        '</ul>',
+                        'Translation' => '<ul class="accordion-ul">' .
+                        '<li>' . CHtml::link('Common', array('/dtMessages/index',
+                            "category" => "common")) . '</li>' .
+                        '<li>' . CHtml::link('Header Footer', array('/dtMessages/index',
+                            "category" => "header_footer")) . '</li>' .
+                        '<li>' . CHtml::link('Labels', array('/dtMessages/index',
+                            "category" => "models_labels")) . '</li>' .
+                        '<li>' . CHtml::link('Product Detail', array('/dtMessages/index',
+                            "category" => "product_detail")) . '</li>' .
+//                        '<li>' . CHtml::link('Product Category', array('/dtMessages/index',
+//                            "category" => "product_category")) . '</li>' .
+                        '</ul>',
+                    );
+
+                    if (!Yii::app()->user->IsSuperuser) {
+                        unset($link_array['Translation']);
+                    }
+                    $this->widget('zii.widgets.jui.CJuiAccordion', array(
+                        'panels' => $link_array,
+                        // additional javascript options for the accordion plugin
+                        'cssFile' => Yii::app()->getClientScript()->registerCssFile(Yii::app()->theme->baseUrl . '/css/jq-aquradian.css'),
+                        'options' => array(
+                            'autoHeight' => false,
+                            'navigation' => true,
+                            'clearStyle' => true,
+                            'resize' => true,
+                        ),
+                        'htmlOptions' => array('style' => 'font-size:12px;margin-top:0')
+                    ));
+                } else {
+                    $this->widget('zii.widgets.CMenu', array(
+                        'items' => $this->menu,
+                        'htmlOptions' => array('class' => 'operations'),
+                    ));
+                }
+                if (isset($this->PcmWidget['filter'])) {
+                    $this->widget($this->PcmWidget['filter']['name'], $this->PcmWidget['filter']['attributes']);
+                    echo "<hr />";
+                }
                 ?>
             </div>
             <br>
