717311f8dac52e5b7962231b63926195614d93a2
php-dev <php-dev@php-dev>
2014-07-10 14:43:55 +0500

    added method to return allowed acl actions

--- protected/modules/rights/RightsModule.php
+++ protected/modules/rights/RightsModule.php
@@ -1,230 +1,256 @@
 <?php
+
 /**
-* Rights module class file.
-*
-* @author Christoffer Niska <cniska@live.com>
-* @copyright Copyright &copy; 2010 Christoffer Niska
-* @version 1.2.0
-* 
-* DO NOT CHANGE THE DEFAULT CONFIGURATION VALUES!
-* 
-* You may overload the module configuration values in your rights-module 
-* configuration like so:
-* 
-* 'modules'=>array(
-*     'rights'=>array(
-*         'userNameColumn'=>'name',
-*         'flashSuccessKey'=>'success',
-*         'flashErrorKey'=>'error',
-*     ),
-* ),
-*/
-class RightsModule extends CWebModule
-{
-	/**
-	* @property string the name of the role with superuser priviledges.
-	*/
-	public $superuserName = 'super@yahoo.com';
-        
-        /**
-         * related to this project only
-         * @var type 
-         */
-        public $cityAdmin;
-	/**
-	* @property string the name of the guest role.
-	*/
-	public $authenticatedName = 'Authenticated';
-	/**
-	* @property string the name of the user model class.
-	*/
-	public $userClass = 'User';
-	/**
-	* @property string the name of the id column in the user table.
-	*/
-	public $userIdColumn = 'user_id';
-	/**
-	* @property string the name of the username column in the user table.
-	*/
-	public $userNameColumn = 'user_name';
-	/**
-	* @property boolean whether to enable business rules.
-	*/
-	public $enableBizRule = true;
-	/**
-	* @property boolean whether to enable data for business rules.
-	*/
-	public $enableBizRuleData = false;
-	/**
-	* @property boolean whether to display authorization items description 
-	* instead of name it is set.
-	*/
-	public $displayDescription = true;
-	/**
-	* @property string the flash message key to use for success messages.
-	*/
-	public $flashSuccessKey = 'RightsSuccess';
-	/**
-	* @property string the flash message key to use for error messages.
-	*/
-	public $flashErrorKey = 'RightsError';
-	/**
-	* @property boolean whether to install rights when accessed.
-	*/
-	public $install = false;
-	/**
-	* @property string the base url to Rights. Override when module is nested.
-	*/
-	public $baseUrl = '/rights';
-	/**
-	* @property string the path to the layout file to use for displaying Rights.
-	*/
-	public $layout = 'rights.views.layouts.main';
-	/**
-	* @property string the path to the application layout file.
-	*/
-	public $appLayout = 'application.themes.abound.views.layout.main';
-	/**
-	* @property string the style sheet file to use for Rights.
-	*/
-	public $cssFile;
-	/**
-	* @property boolean whether to enable debug mode.
-	*/
-	public $debug = false;
+ * Rights module class file.
+ *
+ * @author Christoffer Niska <cniska@live.com>
+ * @copyright Copyright &copy; 2010 Christoffer Niska
+ * @version 1.2.0
+ * 
+ * DO NOT CHANGE THE DEFAULT CONFIGURATION VALUES!
+ * 
+ * You may overload the module configuration values in your rights-module 
+ * configuration like so:
+ * 
+ * 'modules'=>array(
+ *     'rights'=>array(
+ *         'userNameColumn'=>'name',
+ *         'flashSuccessKey'=>'success',
+ *         'flashErrorKey'=>'error',
+ *     ),
+ * ),
+ */
+class RightsModule extends CWebModule {
 
-	private $_assetsUrl;
-               
-        /**
-         *  super actions
-         */
-        
-        public $superAllowedactions;
+    /**
+     * @property string the name of the role with superuser priviledges.
+     */
+    public $superuserName = 'super@yahoo.com';
 
-	/**
-	* Initializes the "rights" module.
-	*/
-	public function init()
-	{
-		// Set required classes for import.
-		$this->setImport(array(
-			'rights.components.*',
-			'rights.components.behaviors.*',
-			'rights.components.dataproviders.*',
-			'rights.controllers.*',
-			'rights.models.*',
-		));
+    /**
+     * related to this project only
+     * @var type 
+     */
+    public $cityAdmin;
 
-		// Set the required components.
-		$this->setComponents(array(
-			'authorizer'=>array(
-				'class'=>'RAuthorizer',
-				'superuserName'=>$this->superuserName,
-			),
-			'generator'=>array(
-				'class'=>'RGenerator',
-			),
-		));
+    /**
+     * @property string the name of the guest role.
+     */
+    public $authenticatedName = 'Authenticated';
 
-		// Normally the default controller is Assignment.
-		$this->defaultController = 'assignment';
+    /**
+     * @property string the name of the user model class.
+     */
+    public $userClass = 'User';
 
-		// Set the installer if necessary.
-		if( $this->install===true )
-		{
-			$this->setComponents(array(
-				'installer'=>array(
-					'class'=>'RInstaller',
-					'superuserName'=>$this->superuserName,
-					'authenticatedName'=>$this->authenticatedName,
-					'guestName'=>Yii::app()->user->guestName,
-					'defaultRoles'=>Yii::app()->authManager->defaultRoles,
-				),
-			));
+    /**
+     * @property string the name of the id column in the user table.
+     */
+    public $userIdColumn = 'user_id';
 
-			// When installing we need to set the default controller to Install.
-			$this->defaultController = 'install';
-		}
-	}
+    /**
+     * @property string the name of the username column in the user table.
+     */
+    public $userNameColumn = 'user_name';
 
-	/**
-	* Registers the necessary scripts.
-	*/
-	public function registerScripts()
-	{
-		// Get the url to the module assets
-		$assetsUrl = $this->getAssetsUrl();
+    /**
+     * @property boolean whether to enable business rules.
+     */
+    public $enableBizRule = true;
 
-		// Register the necessary scripts
-		$cs = Yii::app()->getClientScript();
-		$cs->registerCoreScript('jquery',CClientScript::POS_END);
-		$cs->registerCoreScript('jquery.ui',CClientScript::POS_END);
-		$cs->registerScriptFile($assetsUrl.'/js/rights.js',CClientScript::POS_END);
-		$cs->registerCssFile($assetsUrl.'/css/core.css');
+    /**
+     * @property boolean whether to enable data for business rules.
+     */
+    public $enableBizRuleData = false;
 
-		// Make sure we want to register a style sheet.
-		if( $this->cssFile!==false )
-		{
-			// Default style sheet is used unless one is provided.
-			if( $this->cssFile===null )
-				$this->cssFile = $assetsUrl.'/css/default.css';
-			else
-				$this->cssFile = Yii::app()->request->baseUrl.$this->cssFile;
+    /**
+     * @property boolean whether to display authorization items description 
+     * instead of name it is set.
+     */
+    public $displayDescription = true;
 
-			// Register the style sheet
-			$cs->registerCssFile($this->cssFile);
-		}
-	}
+    /**
+     * @property string the flash message key to use for success messages.
+     */
+    public $flashSuccessKey = 'RightsSuccess';
 
-	/**
-	* Publishes the module assets path.
-	* @return string the base URL that contains all published asset files of Rights.
-	*/
-	public function getAssetsUrl()
-	{
-		if( $this->_assetsUrl===null )
-		{
-			$assetsPath = Yii::getPathOfAlias('rights.assets');
+    /**
+     * @property string the flash message key to use for error messages.
+     */
+    public $flashErrorKey = 'RightsError';
 
-			// We need to republish the assets if debug mode is enabled.
-			if( $this->debug===true )
-				$this->_assetsUrl = Yii::app()->getAssetManager()->publish($assetsPath, false, -1, true);
-			else
-				$this->_assetsUrl = Yii::app()->getAssetManager()->publish($assetsPath);
-		}
+    /**
+     * @property boolean whether to install rights when accessed.
+     */
+    public $install = false;
 
-		return $this->_assetsUrl;
-	}
+    /**
+     * @property string the base url to Rights. Override when module is nested.
+     */
+    public $baseUrl = '/rights';
 
-	/**
-	* @return RightsAuthorizer the authorizer component.
-	*/
-	public function getAuthorizer()
-	{
-		return $this->getComponent('authorizer');
-	}
+    /**
+     * @property string the path to the layout file to use for displaying Rights.
+     */
+    public $layout = 'rights.views.layouts.main';
 
-	/**
-	* @return RightsInstaller the installer component.
-	*/
-	public function getInstaller()
-	{
-		return $this->getComponent('installer');
-	}
+    /**
+     * @property string the path to the application layout file.
+     */
+    public $appLayout = 'application.themes.abound.views.layout.main';
 
-	/**
-	* @return RightsGenerator the generator component.
-	*/
-	public function getGenerator()
-	{
-		return $this->getComponent('generator');
-	}
+    /**
+     * @property string the style sheet file to use for Rights.
+     */
+    public $cssFile;
 
-	/**
-	* @return the current version.
-	*/
-	public function getVersion()
-	{
-		return '1.2.0';
-	}
+    /**
+     * @property boolean whether to enable debug mode.
+     */
+    public $debug = false;
+    private $_assetsUrl;
+
+    /**
+     *  super actions
+     */
+    public $superAllowedactions;
+
+    /**
+     * Initializes the "rights" module.
+     */
+    public function init() {
+        // Set required classes for import.
+        $this->setImport(array(
+            'rights.components.*',
+            'rights.components.behaviors.*',
+            'rights.components.dataproviders.*',
+            'rights.controllers.*',
+            'rights.models.*',
+        ));
+
+        // Set the required components.
+        $this->setComponents(array(
+            'authorizer' => array(
+                'class' => 'RAuthorizer',
+                'superuserName' => $this->superuserName,
+            ),
+            'generator' => array(
+                'class' => 'RGenerator',
+            ),
+        ));
+
+        // Normally the default controller is Assignment.
+        $this->defaultController = 'assignment';
+
+        // Set the installer if necessary.
+        if ($this->install === true) {
+            $this->setComponents(array(
+                'installer' => array(
+                    'class' => 'RInstaller',
+                    'superuserName' => $this->superuserName,
+                    'authenticatedName' => $this->authenticatedName,
+                    'guestName' => Yii::app()->user->guestName,
+                    'defaultRoles' => Yii::app()->authManager->defaultRoles,
+                ),
+            ));
+
+            // When installing we need to set the default controller to Install.
+            $this->defaultController = 'install';
+        }
+    }
+
+    /**
+     * Registers the necessary scripts.
+     */
+    public function registerScripts() {
+        // Get the url to the module assets
+        $assetsUrl = $this->getAssetsUrl();
+
+        // Register the necessary scripts
+        $cs = Yii::app()->getClientScript();
+        $cs->registerCoreScript('jquery', CClientScript::POS_END);
+        $cs->registerCoreScript('jquery.ui', CClientScript::POS_END);
+        $cs->registerScriptFile($assetsUrl . '/js/rights.js', CClientScript::POS_END);
+        $cs->registerCssFile($assetsUrl . '/css/core.css');
+
+        // Make sure we want to register a style sheet.
+        if ($this->cssFile !== false) {
+            // Default style sheet is used unless one is provided.
+            if ($this->cssFile === null)
+                $this->cssFile = $assetsUrl . '/css/default.css';
+            else
+                $this->cssFile = Yii::app()->request->baseUrl . $this->cssFile;
+
+            // Register the style sheet
+            $cs->registerCssFile($this->cssFile);
+        }
+    }
+
+    /**
+     * Publishes the module assets path.
+     * @return string the base URL that contains all published asset files of Rights.
+     */
+    public function getAssetsUrl() {
+        if ($this->_assetsUrl === null) {
+            $assetsPath = Yii::getPathOfAlias('rights.assets');
+
+            // We need to republish the assets if debug mode is enabled.
+            if ($this->debug === true)
+                $this->_assetsUrl = Yii::app()->getAssetManager()->publish($assetsPath, false, -1, true);
+            else
+                $this->_assetsUrl = Yii::app()->getAssetManager()->publish($assetsPath);
+        }
+
+        return $this->_assetsUrl;
+    }
+
+    /**
+     * @return RightsAuthorizer the authorizer component.
+     */
+    public function getAuthorizer() {
+        return $this->getComponent('authorizer');
+    }
+
+    /**
+     * @return RightsInstaller the installer component.
+     */
+    public function getInstaller() {
+        return $this->getComponent('installer');
+    }
+
+    /**
+     * @return RightsGenerator the generator component.
+     */
+    public function getGenerator() {
+        return $this->getComponent('generator');
+    }
+
+    /**
+     * @return the current version.
+     */
+    public function getVersion() {
+        return '1.2.0';
+    }
+
+    public static function getAllowedActions() {
+        $actions = array();
+
+        $user_role = Yii::app()->db->createCommand('select itemname from authassignment WHERE userid = ' . Yii::app()->user->id)->queryRow();
+
+        if (!empty($user_role)) {
+            $sql = "SELECT  ac.child,au.child as child1 from authitemchild " .
+                    " ac LEFT JOIN " .
+                    " authitemchild au ON ac.child = au.parent" .
+                    " WHERE ac.parent = '" . $user_role['itemname'] . "'";
+
+            $actions = Yii::app()->db->createCommand($sql)->queryAll();
+
+            $actions_1 = CHtml::listData($actions, "child", 'child');
+            $actions_2 = CHtml::listData($actions, "child1", 'child1');
+            $actions = array_merge($actions_1, $actions_2);
+        }
+        //  CVarDumper::dump(array_filter($actions), 10, true);  die;
+        return array_filter($actions);
+    }
+
 }
