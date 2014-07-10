7f0ece4ef4d8e9abc5eb236dcd905e0153b2e2ca
php-dev <php-dev@php-dev>
2014-07-10 14:46:51 +0500

    chanages for javascript issues fixed

--- themes/abound/views/site/login.php
+++ themes/abound/views/site/login.php
@@ -5,66 +5,71 @@
 Yii::app()->clientScript->scriptMap['jquery.js'] = true;
 unset(Yii::app()->clientScript->scriptMap['jquery.js']);
 
-$this->pageTitle=Yii::app()->name . ' - Admin Login';
-$this->breadcrumbs=array(
-	'Admin Login',
+$jqueryPath = Yii::app()->getAssetManager()->publish(Yii::getPathOfAlias('system.web.js.source'));
+Yii::app()->clientScript->registerScriptFile($jqueryPath . '/jquery.yiiactiveform', CClientScript::POS_BEGIN);
+
+
+$this->pageTitle = Yii::app()->name . ' - Admin Login';
+$this->breadcrumbs = array(
+    'Admin Login',
 );
 ?>
 <div class="page-header">
-	<h1>Login <small>to your account</small></h1>
+    <h1>Login <small>to your account</small></h1>
 </div>
 <div class="row-fluid">
-	
+
     <div class="span6 offset3">
-<?php
-	$this->beginWidget('zii.widgets.CPortlet', array(
-		'title'=>"Private access",
-	));
-	
-?>
+        <?php
+        $this->beginWidget('zii.widgets.CPortlet', array(
+            'title' => "Private access",
+        ));
+        ?>
 
 
 
-    <p>Please fill out the following form with your login credentials:</p>
-    
-    <div class="form">
-    <?php $form=$this->beginWidget('CActiveForm', array(
-        'id'=>'login-form',
-        'enableClientValidation'=>true,
-        'clientOptions'=>array(
-            'validateOnSubmit'=>true,
-        ),
-    )); ?>
-    
-        <p class="note">Fields with <span class="required">*</span> are required.</p>
-    
-        <div class="row">
-            <?php echo $form->labelEx($model,'username'); ?>
-            <?php echo $form->textField($model,'username'); ?>
-            <?php echo $form->error($model,'username'); ?>
-        </div>
-    
-        <div class="row">
-            <?php echo $form->labelEx($model,'password'); ?>
-            <?php echo $form->passwordField($model,'password'); ?>
-            <?php echo $form->error($model,'password'); ?>
-       
-        </div>
-    
-        <div class="row rememberMe">
-            <?php echo $form->checkBox($model,'rememberMe'); ?>
-            <?php echo $form->label($model,'rememberMe'); ?>
-            <?php echo $form->error($model,'rememberMe'); ?>
-        </div>
-    
-        <div class="row buttons">
-            <?php echo CHtml::submitButton('Login',array('class'=>'btn btn btn-primary')); ?>
-        </div>
-    
-    <?php $this->endWidget(); ?>
-    </div><!-- form -->
+        <p>Please fill out the following form with your login credentials:</p>
 
-<?php $this->endWidget();?>
+        <div class="form">
+            <?php
+            $form = $this->beginWidget('CActiveForm', array(
+                'id' => 'login-form',
+                'enableClientValidation' => true,
+                'clientOptions' => array(
+                    'validateOnSubmit' => true,
+                ),
+            ));
+            ?>
+
+            <p class="note">Fields with <span class="required">*</span> are required.</p>
+
+            <div class="row">
+                <?php echo $form->labelEx($model, 'username'); ?>
+                <?php echo $form->textField($model, 'username'); ?>
+                <?php echo $form->error($model, 'username'); ?>
+            </div>
+
+            <div class="row">
+                <?php echo $form->labelEx($model, 'password'); ?>
+                <?php echo $form->passwordField($model, 'password'); ?>
+                <?php echo $form->error($model, 'password'); ?>
+
+            </div>
+
+            <div class="row rememberMe">
+                <?php echo $form->checkBox($model, 'rememberMe'); ?>
+                <?php echo $form->label($model, 'rememberMe'); ?>
+                <?php echo $form->error($model, 'rememberMe'); ?>
+            </div>
+
+            <div class="row buttons">
+                <?php echo CHtml::submitButton('Login', array('class' => 'btn btn btn-primary')); ?>
+            </div>
+
+            <?php $this->endWidget(); ?>
+        </div><!-- form -->
+
+        <?php $this->endWidget(); ?>
 
     </div>
 
