dba3516936e23cf5636d1a99750398080ee49cbf
Niki John <naeem.mushtaq@dtechsystems.net>
2015-01-13 10:57:31 +0500

    updating the migration

--- protected/migrations/m150105_125050_shippingweighttype.php
+++ protected/migrations/m150105_125050_shippingweighttype.php
@@ -1,27 +1,16 @@
 <?php
 
-class m150105_125050_shippingweighttype extends CDbMigration
+class m150105_125050_shippingweighttype extends DTDbMigration
 {
 	public function up()
 	{
-                $this->alterColumn('shipping_class', 'min_weight_id', 'double');
-                $this->alterColumn('shipping_class', 'max_weight_id', 'double');
+            $this->alterColumn('shipping_class', 'min_weight_id', 'double');
+            $this->alterColumn('shipping_class', 'max_weight_id', 'double');
 	}
 
 	public function down()
 	{
-		echo "m150105_125050_shippingweighttype does not support migration down.\n";
-		return false;
+            $this->alterColumn('shipping_class', 'max_weight_id', 'int');
+            $this->alterColumn('shipping_class', 'min_weight_id', 'int');
 	}
-
-	/*
-	// Use safeUp/safeDown to do migration with transaction
-	public function safeUp()
-	{
-	}
-
-	public function safeDown()
-	{
-	}
-	*/
 }
